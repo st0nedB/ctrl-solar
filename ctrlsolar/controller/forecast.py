bf95edd7b9c5fd621746c3a221547857ad95f9b9
Steffen Schieler <steffen.schieler@hotmail.de>
2025-08-20 10:34:17 +0200

    fix forecast for refactorings

--- ctrlsolar/controller/forecast.py
+++ ctrlsolar/controller/forecast.py
@@ -7,11 +7,16 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class ProductionForecast(Controller):
     name: str = "ProductionForecast"
 
     def __init__(
-        self, panels: list[Panel], weather: OpenMeteoForecast, sensor_today: Sensor, max_age: timedelta = timedelta(hours=1)
+        self,
+        panels: list[Panel],
+        weather: OpenMeteoForecast,
+        sensor_today: Sensor,
+        max_age: timedelta = timedelta(hours=1),
     ):
         self.weather = weather
         self.panels = panels
@@ -20,7 +25,7 @@ class ProductionForecast(Controller):
         self._weather_forecast = None
         self._weather_forecast_from = None
 
-    def forecast_today(self):
+    def _update_weather_forecast(self) -> None:
         if self._weather_forecast is None:
             self._weather_forecast = self.weather.get()
             self._weather_forecast_from = datetime.now()
@@ -30,17 +35,38 @@ class ProductionForecast(Controller):
                     self._weather_forecast = self.weather.get()
                     self._weather_forecast_from = datetime.now()
 
-        weather = self._weather_forecast
-        p_dcs = [x.predicted_production_by_hour for x in self.panels]
+        return
 
+    def production_estimate(self) -> float:
+        p_dcs = [
+            float(x.predicted_production_by_hour.values.sum()) for x in self.panels
+        ]
         return sum(p_dcs)
 
     def production_today(self):
         return self.sensor_today.get()
 
     def update(self):
-        forecast = self.forecast_today()
+        self._update_weather_forecast()
+        forecast = self.production_estimate()
         until_now = self.production_today()
-        forecast_age = (datetime.now() - self._weather_forecast_from).total_seconds() // 60 if self._weather_forecast_from is not None else "N/A"
+        forecast_age = (
+            (datetime.now() - self._weather_forecast_from).total_seconds() // 60
+            if self._weather_forecast_from is not None
+            else "N/A"
+        )
+
+        logger.info(f"--------")
+        logger.info(f"Production Forecast for today (age: {forecast_age:.0f} minutes)")
+        logger.info(
+            "Until now\t\t{x}".format(
+                x=f"{1E-3*until_now:.2f} kWh" if until_now is not None else "N/A"
+            )
+        )
+        logger.info(
+            "Estimated\t\t{x}".format(
+                x=f"{1E-3*forecast:.2f} kWh" if forecast is not None else "N/A"
+            )
+        )
 
         return

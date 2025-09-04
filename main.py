7db10f647e555763297da3dfa40b6be4b00389d3
Steffen Schieler <steffen.schieler@hotmail.de>
2025-09-04 12:53:19 +0200

    remove wrong config path

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def main():
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.INFO)
 
-    # config = Config.from_yaml("/app/config.yaml")
     config = Config.from_yaml("config.yaml")
 
     mqtt = Mqtt(**config.mqtt)

163fe9e7f39898bbd5c113a142c4a858ff898816
Steffen Schieler <steffen.schieler@hotmail.de>
2026-03-24 23:22:37 +0100

    add connection check

--- ctrlsolar/app.py
+++ ctrlsolar/app.py
@@ -3,12 +3,23 @@ import hydra
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 from ctrlsolar.io.mqtt import set_mqtt
+import time
 
 @hydra.main(version_base=None, config_path=".", config_name="defaults")
 def run(config: DictConfig) -> None:
     mqtt = instantiate(config.mqtt)
     mqtt.connect()
     set_mqtt(mqtt)
+
+    # add a connection check with timeout and error raising if conenction fails
+    for ii in range(5):
+        time.sleep(2)
+        if mqtt.client.is_connected():
+            break
+
+        if ii == 4:
+            raise RuntimeError(f"Connection to MQTT broker could not be established.")
+        
     
     loop = instantiate(config.loop)
     loop.run()
